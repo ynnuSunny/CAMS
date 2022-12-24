bb293c6aa3db438e06f3087cd0c02d615bbc9891
Sunny Sutradhar <sunnysutradhar38@gmail.com>
2022-12-25 01:45:24 +0600

    create user app and user model
    create a an app for system user maintenance. And create a custom user model for users. Our app have 3types of user (Company manager, Stuff, Employee). We separate user role based.

--- CAMS/settings.py
+++ CAMS/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+AUTH_USER_MODEL = "users.User" 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
---
+++ users/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+
+
+
+
+#crate an custom user model which is inherite django AbstractUser model for us django built in user model functionality 
+class User(AbstractUser):
+    #CAMS has 3type of user, this role class is basicly for seting role of a user
+    class Role(models.TextChoices):
+        MANAGER = "MANAGER",'Manager'
+        STUFF = "STUFF",'Stuff'
+        EMPLOYEE = "EMPLOYEE",'Employee'
+    
+    #creating some extra field for user table
+    base_role = Role.MANAGER
+    role  =  models.CharField(max_length=50,choices=Role.choices)
+    company_name = models.CharField(max_length=100)
+    company_website = models.CharField(max_length=100)
+    company_id = models.PositiveIntegerField(null=False,blank=False)
+    
+
+    
+    def save(self,*args,**kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args,**kwargs)
+
+
+
+class ManagerManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        result =  super().get_queryset(*args,**kwargs)
+        return result.filter(role=User.Role.MANAGER)
+
+
+class Manager(User):
+    base_role = User.Role.MANAGER
+    manager = ManagerManager()
+    class Meta:
+        proxy = True
+
+    def welcome(slef):
+        return "Only for managers"
+
+class StuffManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        result =  super().get_queryset(*args,**kwargs)
+        return result.filter(role=User.Role.STUFF)
+
+
+class Stuff(User):
+    base_role = User.Role.STUFF
+    stuff = StuffManager()
+    class Meta:
+        proxy = True
+
+    def welcome(slef):
+        return "Only for stuff"
+
+class EmployeeManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        result =  super().get_queryset(*args,**kwargs)
+        return result.filter(role=User.Role.EMPLOYEE)
+
+
+class Employee(User):
+    base_role = User.Role.EMPLOYEE
+    employee = EmployeeManager()
+    class Meta:
+        proxy = True
+
+    def welcome(slef):
+        return "Only for employee"
