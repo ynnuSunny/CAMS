083ea572e3c02fd9dfeca9b88e32f7b1a9b395a9
Sunny Sutradhar <sunnysutradhar38@gmail.com>
2022-12-25 11:01:59 +0600

    Accept, Reject, and Receive Assets

--- assets/urls.py
+++ assets/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     
     path('',views.view_assets,name="view-assets"),
     path('add-assets',views.add_assets,name="add-assets"),
-    path('request-assets',views.request_assets,name="request-assets")
+    path('request-assets',views.request_assets,name="request-assets"),
+    path('view-request',views.view_request,name="view-request"),
+    path('assets-persmission',views.assets_persmission,name="assets-persmission")
     
 ]
--- assets/views.py
+++ assets/views.py
@@ -51,5 +51,59 @@ def request_assets(request):
 
         return render(request,"view-assets.html")
         
+@login_required
+def view_request(request):
+    admin = request.user
+    if(admin.role=="EMPLOYEE"):
+        return HttpResponse("<h1>You are not authorized user!!!<h1>")
+    if request.method=="GET":
+        user = request.user
+        assets = Assets.objects.filter(company_id = user.company_id).filter(request_by__isnull=False)
+        fs = FileSystemStorage()
+        allAssets = []
+        for i in assets:
+            i.image = fs.url(i.image)
+            allAssets.append(i)
+        context = {
+            'assets' : allAssets
+        }
+        return render(request,"view-request.html",context)
 
+    if(request.method == "POST"):
+        company_id = admin.company_id
+        assets_name = request.POST['assets-name']
+        uploaded_file = request.FILES['image']
+        fs = FileSystemStorage()   
+        image=fs.save(uploaded_file.name, uploaded_file)
+        assets = Assets(company_id=company_id,assets_name=assets_name,image=image)
+        assets.save()
 
+        return redirect("view-assets")
+
+@login_required
+def assets_persmission(request):
+    admin = request.user
+    if(admin.role=="EMPLOYEE"):
+        return HttpResponse("<h1>You are not authorized user!!!<h1>")
+
+    if(request.method=="POST"):
+        manager = request.user
+        id = request.POST['id']
+        if(request.POST['value'] == "ACCEPT"):
+            assets = Assets.objects.get(id=id)
+            assets.request_accepted_by = manager.id
+            assets.save()
+            return redirect("view-request")
+        if(request.POST['value'] == "REJECT"):
+            assets = Assets.objects.get(id=id)
+            assets.request_by = None
+            assets.save()
+            return redirect("view-request")
+        if(request.POST['value'] == "RECEIVE"):
+            assets = Assets.objects.get(id=id)
+            assets.request_accepted_by = None
+            assets.request_by = None
+            assets.save()
+            return redirect("view-request")
+        
+        
